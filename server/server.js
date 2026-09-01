95a4b8d7398fa3dfca6208b5da0892c8550f40d4
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-02 00:31:34 +0530

    feat: initialize backend server structure with Express, Mongoose, and base MVC architecture

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const app = express();
+require('dotenv').config()
+app.use(express.json()); //for parsing the req.body
+
+app.get('/', (req, res) => {
+    res.send('Hello from backend!')
+})
+
+
+const server = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_CONN_STR)
+        console.log(" MongoDB Connected successfully")
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running at http://localhost:${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+server();
+
 
