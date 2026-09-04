ffe88da2aaaf3484c4b93ee8e25d749ed7df6b57
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-04 17:43:29 +0530

    feat: initialize server dependencies and implement Cloudinary image upload service

---
+++ server/services/cloudinaryService.js
@@ -0,0 +1,19 @@
+const cloudinary = require('cloudinary')
+
+//To take an image buffer from Multer (req.file.buffer) and upload it to Cloudinary
+const uploadImage = (buffer)=>{
+    return new Promise ((resolve,reject)=>{
+        const uploadStream = cloudinary.uploader.upload_stream({
+            folder: "plants",
+            resource_type: "image"
+        },(error,result)=>{
+            if(error){
+                reject(error);
+            }else{
+                resolve(result); //result contains {secure_url,public_id} 
+            }
+        })
+        uploadStream.end(buffer)
+    })
+}
+module.exports = uploadImage
