c916d952632c7b4a316f6aa0b49ddcc057f43f85
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-05 18:45:17 +0530

    import Plant Model, change the export style of identifyPlant controller, add comments to describe the steps and working

--- server/controllers/plantController.js
+++ server/controllers/plantController.js
@@ -1,16 +1,19 @@
 const path = require('path');
+const Plant = require('../models/plantModel')
 const CustomError = require('./../utils/CustomError')
 const cloudinaryService = require('./../services/cloudinaryService')
 const geminiService = require('./../services/geminiService')
 const asyncErrorHandler = require('./../utils/asyncErrorHandler')
 
-const identifyPlant = asyncErrorHandler(async (req, res, next) => {
+//identification of plant via Gemini API with cloudinary storage
+exports.identifyPlant = asyncErrorHandler(async (req, res, next) => {
     if (!req.file) return next(new CustomError("Please upload your plant image", 400))
 
     const fileBuffer = req.file.buffer;
     let mimeType = req.file.mimetype;
 
     // Fallback if client/Postman sends generic octet-stream
+    //This happens because the front end is sending the request as multipart/form-data, and Multer extracts the image buffer
     if (!mimeType || !mimeType.startsWith('image/')) {
         const ext = path.extname(req.file.originalname).toLowerCase();
         if (ext === '.png') mimeType = 'image/png';
@@ -18,13 +21,13 @@ const identifyPlant = asyncErrorHandler(async (req, res, next) => {
         else if (ext === '.webp') mimeType = 'image/webp';
         else mimeType = 'image/jpeg';
     }
-
+    
     //1. Upload to cloud
-    const cloudinaryResult = await cloudinaryService.uploadImage(fileBuffer);
-    const imageUrl = cloudinaryResult.secure_url;
+    const cloudinaryResult = await cloudinaryService.uploadImage(fileBuffer); //filebuffer contains the binary data of the image
+    const imageUrl = cloudinaryResult.secure_url; //url returned by cloudinary to display the image in frontend
 
     //2. pass buffer to Gemini for analysis
-    const aiResult = await geminiService.analysePlantImage(fileBuffer, mimeType);
+    const aiResult = await geminiService.analysePlantImage(fileBuffer, mimeType); //mimeType tells Gemini what type of data the buffer contains (e.g., image/jpeg, image/png) so it can decode and interpret the image correctly
 
     //3. Return the preview data
     return res.status(200).json({
@@ -60,5 +63,3 @@ const identifyPlant = asyncErrorHandler(async (req, res, next) => {
         },
     })
 })
-
-module.exports = identifyPlant
