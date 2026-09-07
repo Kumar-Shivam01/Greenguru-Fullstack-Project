a2654269dbba0a44c968bd50ad0415e3984c8971
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-07 19:02:11 +0530

    feat: initialize React client project with Vite and Tailwind CSS configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 function App() {
-
-}
+  return(
+    <div className="min-h-screen bg-green-950 flex items-center justify-center"> 
+      <h1 className="text-5xl font-semibold text-white">Green Guru</h1>
+    </div>
+  )
+}  
 
 
 export default App
