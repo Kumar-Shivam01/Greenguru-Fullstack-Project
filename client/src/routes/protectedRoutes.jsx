585464c89cb5b270ca29b844a5d919e345580587
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-09 19:23:50 +0530

    install react-icons and  apply style to loading div

--- client/src/routes/protectedRoutes.jsx
+++ client/src/routes/protectedRoutes.jsx
@@ -2,7 +2,7 @@ import { Navigate,Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 function ProtectedRoute(){
     const {user,loading} = useAuth();
-    if(loading) return <div>Loading...</div>
+    if(loading) return <div className="flex min-h-screen items-center justify-center">Loading...</div>
     
     if(!user) return <Navigate to="/login" replace/>;
     return <Outlet/>;
