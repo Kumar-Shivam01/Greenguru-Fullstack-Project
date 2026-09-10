d78844fa553b83632332d94a2925e3e4c05bebb7
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-10 21:17:07 +0530

    fix(api): remove default Content-Type header from axiosClient

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -2,8 +2,5 @@ import axios from 'axios'
 const axiosClient = axios.create({
     baseURL: "http://localhost:8001/api/v1",
     withCredentials: true, //include cookies when making requests to my backend
-    headers:{
-        "Content-Type": "application/json"
-    }
 })
 export default axiosClient
