bea09b037602d2b9194b589cb75593262985b44c
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-14 02:41:52 +0530

    feat: add changePassword API to change user's current password

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -8,3 +8,7 @@ export const updateUserProfile = async (profileData)=>{
     const response = await axiosClient.patch('/user/profile',profileData)
     return response.data
 }
+export const changePassword = async(passwordData)=>{
+    const response = await axiosClient.patch('/user/change-password',passwordData)
+    return response.data
+}
