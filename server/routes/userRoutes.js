78ff32c43ff736f25fbadd603b6dece214fea52d
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-14 02:36:36 +0530

    feat: add a new user route /change-password  for changing the user's current password

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const protectedRoute = require('./../middlewares/protectedRoute')
 const express = require('express')
-const {updateUserProfile,getUserData} = require('./../controllers/userController')
+const {updateUserProfile,getUserData,changePassword} = require('./../controllers/userController');
 
 const userRouter = express.Router();
 userRouter.route('/user-data').get(protectedRoute,getUserData)
 userRouter.route('/profile').patch(protectedRoute,updateUserProfile)
+userRouter.route('/change-password').patch(protectedRoute,changePassword)
 module.exports = userRouter 
