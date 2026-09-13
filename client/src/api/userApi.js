ff8add28de05c200a4aa7c1990247d98d29c7b20
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-13 23:47:43 +0530

    feat: add an API for editing user profile

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -4,3 +4,7 @@ export const getUserData = async () => {
     const response = await axiosClient.get('/user/user-data')
     return response.data
 };
+export const updateUserProfile = async (profileData)=>{
+    const response = await axiosClient.patch('/user/profile',profileData)
+    return response.data
+}
