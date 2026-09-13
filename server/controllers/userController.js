9be05b626f3c7d6274862aeaddedce962374dad7
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-14 02:46:57 +0530

    feat: implement controller for chainging user password -checks if the user's password matches with the given password -sets the user password to the new password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -31,4 +31,20 @@ const updateUserProfile = asyncErrorHandler(async (req,res,next)=>{
         }
     })
 })
-module.exports = {getUserData,updateUserProfile}
+const changePassword = asyncErrorHandler(async (req,res,next)=>{
+    const {currentPassword, newPassword} = req.body;
+    if(!currentPassword || !newPassword) return next(new CustomError('Current and new password are required',400))
+    
+    const user = await User.findById(req.userId).select('+password');
+    const isCurrentPasswordValid = await user.comparePassword(currentPassword,user.password);
+    if(!isCurrentPasswordValid) return next(new CustomError('Your current password is incorrect',400))
+    
+    user.password = newPassword;
+    await user.save();
+    res.status(200).json({
+        success: true,
+        message: 'Password changed successfully'
+    })
+
+})
+module.exports = {getUserData,updateUserProfile,changePassword}
