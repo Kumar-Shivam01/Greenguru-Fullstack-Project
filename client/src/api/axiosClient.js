d21e0bcb2f0d2e1d395dd6f4a987390839608c49
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-19 22:45:18 +0530

    add local url in the base url in the axios configuration

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8001/api/v1',
     withCredentials: true, //include cookies when making requests to my backend
 })
 export default axiosClient
